f34993b3f56f154dd02ab1ebaf164883f19c3628
Ilya Emelyanov <baralgin38@yandex.ru>
2021-10-11 13:01:26 +0300

    Меняет Root на PrivateRoot
    - Переход на страницу с избранным осуществляется через PrivateRoot.
      Страницу с избранным должны видеть только авторизированные
      пользователи, если пользователь не авторизирован, при попытке перейти
      на страницу с избранным его перенаправляет на страницу авторизации.

--- project/src/const.ts
+++ project/src/const.ts
@@ -3,5 +3,10 @@ export enum AppRoute {
   SignIn = '/login',
   Favorites = '/favorites',
   Room = '/offer/:id?',
+  RoomNotLogged = '/offer-not-logged/:id?',
 }
 
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+}
